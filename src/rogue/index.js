d8df36e988f7232d10573865382042e4ab2dd305
Alice Russell <alice@heysparkbox.com>
2021-08-18 09:01:10 -0500

    initial exercise finished

--- src/rogue/index.js
+++ src/rogue/index.js
@@ -1,10 +1,21 @@
 import Character from '../character';
 
-class Rogue {
-  talk() {
+class Rogue extends Character {
+  constructor(name = '') {
+    super(name);
+    this.timesAttacked = 0;
   }
 
+  talk() { return super.talk().toLowerCase().replace('!', '.'); }
+
   attack() {
+    this.timesAttacked += 1;
+    if (this.isNthAttack(5)) this.health -= 5;
+    return (this.isNthAttack(5)) ? 1 : 5;
+  }
+
+  isNthAttack(n) {
+    return (this.timesAttacked % n) === 0;
   }
 }
 
--- src/wizard/index.js
+++ src/wizard/index.js
@@ -1,11 +1,21 @@
 import Character from '../character';
 
-class Wizard {
-  talk() {
+class Wizard extends Character {
+  constructor(name = '') {
+    super(name);
+    this.timesAttacked = 0;
+    this.standardDamage = 1;
   }
 
+  talk() { return super.talk().toUpperCase(); }
+
   attack() {
+    this.timesAttacked += 1;
+    if (!this.isUnderProtection()) this.health -= 10;
+    return this.standardDamage * (this.isUnderProtection()) ? 2 : 1;
   }
+
+  isUnderProtection() { return this.timesAttacked <= 3; }
 }
 
 export default Wizard;
